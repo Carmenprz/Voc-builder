74045afc246c4553a518dd3c6afd6305b9c4bd4e
Carmenprz <carmenp.gomez99@gmail.com>
2021-06-11 20:11:24 +0200

    card semanthic structure

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,13 +1,30 @@
 <template>
   <div>
-      <p>I'm a card</p>
+    <header>
+        <h1>{{ card.word }}</h1>
+    </header>
+    <section>
+        <h2>{{ card.meaning }}</h2>
+    </section>
+    <footer>
+        <span>Card number {{ card.id }}</span>
+    </footer>
   </div>
 </template>
 
 <script>
 export default {
-    
+    data() {
+        return {
+            card: {
+                id: 1,
+                word: 'house',
+                meaning: 'casa'
+            }
+        }
+    }
 }
+
 </script>
 
 <style>
