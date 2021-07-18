1bff3a2f0f0e235184dc5dbec798ad932bbd6e87
Carmenprz <carmenp.gomez99@gmail.com>
2021-07-18 15:08:08 +0200

    style props app

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,20 @@
 <template>
   <div id="app">
-    <h1>Vocabulary Builder</h1>
-    <div id="nav">
-      <router-link to="/">Your Cards</router-link>
-    </div>
     <router-view />
   </div>
 </template>
 
 <style>
+@import url('https://fonts.googleapis.com/css2?family=Lexend+Deca&display=swap');
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: 'Lexend Deca', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+  padding: 10px;
+  padding-left: 15px;
 }
 
 #nav {
@@ -28,5 +28,23 @@
 
 #nav a.router-link-exact-active {
   color: #42b983;
+}
+
+h1 {
+    font-size: 49px;
+    font-style: normal;
+    font-weight: 600;
+    line-height: 61px;
+    letter-spacing: 0em;
+    text-align: left;
+}
+
+.logo {
+    font-size: 24px;
+    font-style: normal;
+    font-weight: 400;
+    line-height: 30px;
+    letter-spacing: 0em;
+    text-align: left;
 }
 </style>
